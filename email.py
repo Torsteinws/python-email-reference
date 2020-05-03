751860197e0c4b7d669c63a98769b0c58abda251
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-05-03 22:46:19 +0200

    function that will read mailing-list.txt, and return email a addresses in the file

--- email.py
+++ email.py
@@ -1,14 +1,26 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 
-def send_email():
-    print('Sending email...')
 
+def get_mail_list() -> List[str]:
+    emails = [] 
+
+    with open('mailing-list.txt', 'r') as file:
+        for line in file:
+            email = line.strip()
+            emails.append(email)
+
+    return emails
+
+def send_email():
+    
     load_dotenv()
     username = os.getenv('EMAIL_USERNAME')
     password = os.getenv('EMAIL_PASSWORD')
 
-    print(username, password)
+    emails = get_mail_list()
+    print(emails)
 
 if __name__ == "__main__":
     os.system('cls')
