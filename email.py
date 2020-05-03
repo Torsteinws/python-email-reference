8f8c92208602cea9008a9a71ae0ec495a2fce0c5
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-05-03 21:34:49 +0200

    Setup of local enviroment variables

--- email.py
+++ email.py
@@ -1,8 +1,14 @@
 import os
+from dotenv import load_dotenv
 
 def send_email():
     print('Sending email...')
-    pass
+
+    load_dotenv()
+    username = os.getenv('EMAIL_USERNAME')
+    password = os.getenv('EMAIL_PASSWORD')
+
+    print(username, password)
 
 if __name__ == "__main__":
     os.system('cls')
